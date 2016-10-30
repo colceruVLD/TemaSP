b67c341d84b89d03116c86bd212403e26eb43a4f
colceruVLD <colceruvlad@yahoo.com>
2016-10-30 21:03:02 +0200

    Add files via upload

---
+++ E1.m
@@ -0,0 +1,8 @@
+a=[0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2] 
+%am initiatilizat vectorul a, vector linie, cu 21 de elemente
+b=[1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1] 
+%am initializat vectorul b, vector coloana,cu 21 de elemente pentru a avea loc inmultirea dintre a si b
+R1=a*b %R1 reprezinta rezultatul inmultirii dintre a si b
+R2=b*a %R2 reprezinta rezultatul inmultirii dintre b si a
+R3=a.*a %R3 reprezinta rezultatul inmultirii element cu element a vectorului a
+R4=b.*b %R4 reprezinta rezultatul inmultirii element cu element a vectorului b
